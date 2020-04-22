fa6b1f735c9e0255654f13ffdcc7981e6d6a900c
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-22 03:49:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 
 def oxford_comma(array)
   if array.length <= 1
-    return array
+    return array.join(", ")
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
