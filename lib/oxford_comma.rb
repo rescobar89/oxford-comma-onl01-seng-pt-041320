1ff48b26821d2cdbeb16cdfadfbd25f9014e4d43
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-22 03:45:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,19 @@
 element_1 = ["kiwi"]
 def oxford_comma(array)
-  # if array == 1
-    
-    
-  result = array.join(" and ")
-    return result
-  # if array < 2
-    result.join(", ")
-    return result
+  if array <= 1
+    return array
+  elsif array == 2
+    array.join(" and ")
+    return array
+  elsif >= 3
+    return array.join(", and")
   end
+    # result = array.join(" and ")
+    # return result
+  # if array < 2
+  #   result.join(", ")
+  #   return result
+  # end
 end
 #   if result < 3
       
