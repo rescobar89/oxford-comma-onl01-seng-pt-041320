2696a4617469001cfd5e89405114d47f3475f3cc
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-22 03:40:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
+element_1 = ["kiwi"]
 def oxford_comma(array)
-
+  if array == 1
+    
+    
+  # result = array.join(" and ")
+  #   return result
+  # if array < 2
+  #   result.join(", ")
+  #   return result
+  # end
 end
+#   if result < 3
+      
+#     result.join(", starfruit")
+#     return result
+# end
+
+# 
