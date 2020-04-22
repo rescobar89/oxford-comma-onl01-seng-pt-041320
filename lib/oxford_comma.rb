46041c88d31db21f9f8d721427fe9a11d895ecd5
Rocio X Escobar <rocio.x.escobar@gmail.com>
2020-04-22 03:57:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.length <= 1
+    return array.join(", ")
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    return array[0..-2].join(", ") + ", and " + array[-1]
+  end
+  
 end
